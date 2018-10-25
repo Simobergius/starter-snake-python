8c85322f564811881e4c8be03d17e69b88639776
Simo Bergius <simo.bergius1@gmail.com>
2018-10-25 09:25:11 +0300

    finish test case

--- app/testing.py
+++ app/testing.py
@@ -10,10 +10,10 @@ data = {
                 {'x': 3, 'y': 6},
                 {'x': 3, 'y': 5},
                 {'x': 3, 'y': 4},
-                {'x': 4, 'y': 6},
-                {'x': 5, 'y': 6},
-                {'x': 6, 'y': 6},
-                {'x': 7, 'y': 6}
+                {'x': 4, 'y': 4},
+                {'x': 5, 'y': 4},
+                {'x': 6, 'y': 4},
+                {'x': 7, 'y': 4}
                ], 
                'health': 100,
                'id': 'testsnake',
@@ -21,10 +21,31 @@ data = {
                'name': 'testsnake',
                'object': 'snake'
             }
+        ],
+        'height': 10,
+        'width': 10,
+        'food': [
+                {'x': 2, 'y': 2},
         ]
     },
     'you': {
-        ## TODO: Add something
+        'body': [
+            {'x': 5, 'y': 5}, 
+            {'x': 5, 'y': 6},
+            {'x': 4, 'y': 6},
+            {'x': 3, 'y': 6},
+            {'x': 3, 'y': 5},
+            {'x': 3, 'y': 4},
+            {'x': 4, 'y': 6},
+            {'x': 5, 'y': 6},
+            {'x': 6, 'y': 6},
+            {'x': 7, 'y': 6}
+        ], 
+        'health': 100,
+        'id': 'testsnake',
+        'length': 10,
+        'name': 'testsnake',
+        'object': 'snake'
     }
 }        
         
@@ -38,3 +59,6 @@ data = {
 #  turn: number;
 #  food: List<Point>;
 #}
+snake = snake.snake()
+
+print snake.doAction(data)
